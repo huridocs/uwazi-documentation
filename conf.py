3fc27284a90c6c777df37550a3dca3ee0fb86a6b
Federico Nocetti <fnocetti@users.noreply.github.com>
2020-05-20 19:18:27 -0300

    Minor fixes

--- conf.py
+++ conf.py
@@ -63,6 +63,11 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_css_files = [
+    'styles.css',
+    'https://fonts.googleapis.com/css?family=Ubuntu%7CRoboto&amp;ver=5.3.3'
+]
+
 html_favicon = 'favicon.png'
 
 def setup(app):
